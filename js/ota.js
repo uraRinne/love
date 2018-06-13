7864dbdb21ea7f2cf07dfaf8aaa4326c5944677f
uraRinne <31657300+uraRinne@users.noreply.github.com>
2018-06-14 03:19:45 +0800

    Add files via upload

---
+++ js/ota.js
@@ -0,0 +1,54 @@
+function y_ipad(){
+ var ua = navigator.userAgent.toLowerCase();
+ if(ua.match(/iPad/i)=="ipad" || ua.match(/iphone/i)=="iphone" || ua.match(/android/i)=="android")
+ {
+  //移动端处理
+ }
+}
+function hidebox()
+{
+$('.txtbox').hide(500);
+}
+function posttxt()
+{
+if($('#text').val()=="")
+{
+	return;
+}
+else
+{
+	$.post("post.php", { name: $('#name').val(), emails: $('#emails').val(),text:$('#text').val() },
+	function(data)
+	{
+		$('#text').val('');
+		getlist(1);
+	});
+}
+}
+function getlist(num){
+$.post("get.php",{page:num}, function(data) {
+  $(".txtlist").html('<li class="pdtitle"></li>'+data);
+  $imgs = $('.oimg');
+  $emailcc=$('.email');
+  for(var i_i=0;i_i<$imgs.length;i_i++)
+  {
+    $emailcc[i_i].id="email_"+i_i;
+	$imgs[i_i].src='https://www.gravatar.com/avatar/'+hex_md5($('#email_'+i_i).attr('yv'))+'?s=128&d=&r=G';
+  }
+  $(".pdtitle").html("Message Board <b>world line:"+$("#ctime").html()+"</b><div class='x' onclick='hidebox()'>x</div>");
+});
+ $('.txtbox').show(500);
+}
+
+var text="Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.";
+var delay=50;
+var i=0;
+function scrollit(){
+txt.innerText=text.slice(0,i++);
+if(i>text.length){
+    i=0;
+	$('#fos').fadeIn(4000);
+    return;
+}
+else setTimeout("scrollit()",delay);
+}
